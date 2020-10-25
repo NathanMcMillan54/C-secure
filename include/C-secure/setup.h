be110f4f03360410163dd6e8f360bf1d526c0fc0
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2020-10-25 00:47:56 -0400

    Initial commit.

---
+++ include/C-secure/setup.h
@@ -0,0 +1,8 @@
+#ifdef _SETUP_H
+#define _SETUP_H
+
+void setup() {
+
+}
+
+#endif /* _SETUP_H */
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include "include/C-secure/setup.h"
+
+void setup_app() {
+    printf("Setting up C-secure chat app...\n");
+}
+
+int main() {
+    printf("C-secure\n");
+    printf("Starting setup...\n");
+    setup_app();
+    return 0;
+}
